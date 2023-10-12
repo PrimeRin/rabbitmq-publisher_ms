c05f39acfe529567d8de37aac2ba0e95de25acdf
PrimeRin <primedruk312@gmail.com>
2023-10-12 15:23:35 +0600

    edit controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,17 @@
 class MessagesController < ApplicationController
   def publish
-    connection = Bunny.new
+    connection = Bunny.new(hostname: 'localhost', port: 5672, username: 'guest', password: 'guest')
     connection.start
 
     channel = connection.create_channel
-    exchange = channel.fanout('logs')
+    exchange = channel.fanout('messages')
+
+    # Create a queue and specify a name
+    queue_name = 'messages'
+    queue = channel.queue(queue_name, durable: true)
+
+    # Bind the queue to the exchange
+    queue.bind(exchange)
 
     message = 'Hello, Microservice 2!'
     exchange.publish(message)
